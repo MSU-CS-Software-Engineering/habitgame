cf15444b83f7c7d69463d339ad58eae009a15ab1
matthewduff <duffinopolis@gmail.com>
2014-08-26 19:30:03 -0500

    Making name change

---
+++ conflict_test.py
@@ -0,0 +1,38 @@
+"""The point of this exercise is to create a merging conflict.
+Accomplish this by deleting the line containing the if statement
+above your name. Only modify the one if statement with your name"""
+
+
+##  BEFORE
+##  if("name"):
+##     print "name"
+##
+##  AFTER
+##  print "name"
+
+
+def print_name():
+    
+    if("Timothy"):
+        print "Timothy"
+        
+    if("Chris W."):
+        print "Chris W."
+        
+    if("Sean"):
+        print "Sean"
+        
+    if("Chris R."):
+        print "Chris R."
+        
+    if("Bryan"):
+        print "Bryan"
+        
+    print "Matthew"
+        
+    if("Dustin"):
+        print "Dustin"
+
+    if("Darron"):
+        print "Darron"
+        
