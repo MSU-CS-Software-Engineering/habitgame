aa2d9a122da3e2b16934e9af9da260c6ba8c5a8d
S-Porter <dreadpiratesean@gmail.com>
2014-09-17 21:55:05 -0500

    Login and dropbox linked
    run the login file from IDLE and it returns db, a dropbox object. you
    can mess around w/ the dropbox functions by calling
    db.client.method_name

--- authenticate.py
+++ authenticate.py
@@ -1,121 +1,199 @@
-
 """
-This module handles authorization and sync for the dropbox storage. How to use:
-When we start the program, call the main function and assign it to a variable (we
+This class handles authorization and sync for the dropbox storage. How to use:
+When we start the program, instantiate the class and assign it to a variable (we
 need to have a copy of the dropbox client to pass later).
-
-client = dropbox_auth()
-
-Once we have the client, any sync requests will need to have the client obj passed too:
-
-sync_files(client, file_name)
+  
+client = db()
+  
+client.dropbox_start() will start the process. If there's already a token in a
+local file, it simply created the dropbox client.
+  
+client.sync_files(file_name) will sync between local and remote files.
 """
- 
+   
 import dropbox
 import webbrowser
 import os.path
 import time
- 
- 
-def sync_files(client, file_name='character.xml'):
-    """
-    Pass this function a dropbox client and it will compare the local file
-    with the copy on the user's dropbox. it replaces the older file with the
-    contents of the newer.
-    """
-     
-    local_modified = time.gmtime(os.path.getmtime(file_name))
- 
-    dropbox_file = client.metadata('//' + file_name)
-    db_modified = time.strptime(dropbox_file.get('modified')[:-5] + 'GMT', '%a, %d %b %Y %H:%M:%S %Z')
- 
-    print 'local modified: %s' % local_modified
-    print 'dpbox modified: %s' % db_modified
- 
-    # to this point, the timestamps are in python time_struct form. this if-statement
-    # converts them to seconds since the unix epoch and subtracts them to find the newest.
-    if time.mktime(local_modified) - time.mktime(db_modified) > 0:
-        print 'local file is more recent, updating dropbox file'
- 
-        client.file_delete('//' + file_name)
-        file = open(file_name)
-        client.put_file('//' + file_name, file)
-    else:
-        print 'dropbox file is more recent, overwriting local file'
- 
-        out = open(file_name, 'wb')
-        with client.get_file('//' + file_name) as f:
-            out.write(f.read())
-         
-     
-def dropbox_auth():
-    """
-    Returns a dropbox client object. the client object gives control of the user's
-    dropbox and contains methods to save/push files, get user info, etc. When this
-    is finished executing, it ensures that there is a file both locally and on db,
-    and performs an initial sync of data.
-    """
-     
-    file_name = 'character.xml'
-    key = 'q5r7dogatg3ruh0'
-    secret = 'ch340x6gxyqk8ie'
- 
-    # check to see if there's an xml file already. if so, no need to go through the whole flow 
-    if os.path.isfile(file_name):
-         
-        #TODO: use XML parsing instead of reading the file normally
-         
-        file = open(file_name)
-        line = file.readline()
-        access_token = line.strip()
-        #access_token = 'xoH35mr0iRwAAAAAAAAAI0OZOexRWxqEKabFZRlf6m2WY3j8xkLEcwAEpV297oXv'
-        client = dropbox.client.DropboxClient(access_token)
- 
-        # if there's a local file but no remote, push the local to db
-        # this shouldn't really ever happen, but just in case.
-        if not client.search('//', file_name):
-            file = open(file_name, 'r')
-            client.put_file('//' + file_name, file)
-            file.close()
+
+
+class db:
+
+
+    def set_code(self, new_code):
+        self.code = new_code
+
+    
+    def sync_files(self, file_name='character.xml'):
+        """
+        Call this function and it will compare the local file
+        with the copy on the user's dropbox. it replaces the older file with the
+        contents of the newer.
+        """
+           
+        local_modified = time.gmtime(os.path.getmtime(self.file_name))
+       
+        dropbox_file = self.client.metadata('//' + self.file_name)
+        db_modified = time.strptime(dropbox_file.get('modified')[:-5] + 'GMT', '%a, %d %b %Y %H:%M:%S %Z')
+       
+        print('local modified: ', local_modified)
+        print('dpbox modified: ', db_modified)
+      
+        # To this point, the timestamps are in python time_struct form. this if-statement
+        # converts them to seconds since the unix epoch and subtracts them to find the newest.
+        if time.mktime(local_modified) - time.mktime(db_modified) > 0:
+            print('local file is more recent, updating dropbox file')
+       
+            self.client.file_delete('//' + self.file_name)
+            file = open(self.file_name)
+            self.client.put_file('//' + self.file_name, file)
+        elif time.mktime(local_modified) - time.mktime(db_modified) < 0:
+            print('dropbox file is more recent, overwriting local file')
+       
+            out = open(self.file_name, 'wb')
+            with self.client.get_file('//' + self.file_name) as f:
+                out.write(f.read())
         else:
-            # if there's a local AND and db file, sync to use the most recent.
-            sync_files(client, file_name)
-         
-    else:
-        # no local file, get access_token from website instead
-         
-        flow = dropbox.client.DropboxOAuth2FlowNoRedirect(key, secret)
-        authorize_url = flow.start()
+            print('The 2 files have the same timestamp, no sync required')
+
+               
+    def dropbox_start(self):
+        """
+        starts the dropbox process, then control will be passed back to the login window
+        """
+
+        self.file_name = 'character.xml'
+        key = 'q5r7dogatg3ruh0'
+        secret = 'ch340x6gxyqk8ie'
+        self.client = ''
      
-        print '1. Go to your browser, a new tab should have opened: '
-        web = webbrowser.open(authorize_url, new=2, autoraise=True)
-        print '2. Click "Allow" (you might have to log in first)'
-        print '3. Copy the authorization code.'
-        code = raw_input("Enter the authorization code here: ").strip()
- 
-        access_token, user_id = flow.finish(code)
-        client = dropbox.client.DropboxClient(access_token)
-        print access_token
- 
+        # check to see if there's an xml file already. if so, no need to go through the whole flow 
+        if os.path.isfile(self.file_name):
+               
+            #TODO: use XML parsing instead of reading the file line by line
+               
+            file = open(self.file_name)
+            line = file.readline()
+            access_token = line.strip()
+     
+            self.client = dropbox.client.DropboxClient(access_token)
+       
+            # if there's a local file but no remote, push the local to db
+            # this shouldn't really ever happen, but just in case.
+            if not self.client.search('//', self.file_name):
+                file = open(self.file_name, 'r')
+                self.client.put_file('//' + self.file_name, file)
+                file.close()
+            else:
+                # if there's a local AND and db file, sync to use the most recent.
+                self.sync_files(self.file_name)
+               
+        else:
+            # no local file, get access_token from website instead
+               
+            self.flow = dropbox.client.DropboxOAuth2FlowNoRedirect(key, secret)
+            authorize_url = self.flow.start()
+              
+            web = webbrowser.open(authorize_url, new=2, autoraise=True)
+
+        
+
+    def dropbox_finish(self, code):
+        """
+        Call this function to finish the dropbox flow and create a dropbox clients
+        (after the user pastes their authorization code in the login screen).
+        """
+        
+        access_token, user_id = self.flow.finish(code)
+        self.client = dropbox.client.DropboxClient(access_token)
+        print(access_token)
+   
         # if db is empty too, new user w/ new install. create local xml file and push to db
-        if not client.search('//', file_name):
-            file = open(file_name, 'wb')
+        if not self.client.search('//', self.file_name):
+            file = open(self.file_name, 'wb')
             # TODO: file creation function goes here, placeholder write
             # init_file(file)
             file.write(access_token)
             file.close()
- 
+   
             # for some reason open(file_name, 'r+') (read AND write) wouldn't work, so I had to
             # open and close separately for the read and writes.
-             
-            file = open(file_name, 'r')
-            client.put_file('//' + file_name, file)
+               
+            file = open(self.file_name, 'r')
+            self.client.put_file('//' + self.file_name, file)
             file.close()
         else:
             # file on db, so this is an existing user w/ a new install
-            file = open(file_name, 'wb')
-            with client.get_file('//' + file_name) as db_file:
+            file = open(self.file_name, 'wb')
+            with self.client.get_file('//' + self.file_name) as db_file:
                 file.write(db_file.read())
             file.close()
- 
-    return client
+           
+    """def dropbox_auth(self):
+        
+        Processes tokens and logins to set the client class var to a dropbox client.
+        the client object gives control of the user's dropbox and contains methods to
+        save/push files, get user info, etc. When this is finished executing, it ensures
+        that there is a file both locally and on db, and performs an initial sync of data.
+        
+           
+        file_name = 'character.xml'
+        key = 'q5r7dogatg3ruh0'
+        secret = 'ch340x6gxyqk8ie'
+     
+     
+        # check to see if there's an xml file already. if so, no need to go through the whole flow 
+        if os.path.isfile(file_name):
+               
+            #TODO: use XML parsing instead of reading the file line by line
+               
+            file = open(file_name)
+            line = file.readline()
+            access_token = line.strip()
+     
+            client = dropbox.client.DropboxClient(access_token)
+       
+            # if there's a local file but no remote, push the local to db
+            # this shouldn't really ever happen, but just in case.
+            if not client.search('//', file_name):
+                file = open(file_name, 'r')
+                client.put_file('//' + file_name, file)
+                file.close()
+            else:
+                # if there's a local AND and db file, sync to use the most recent.
+                self.sync_files(file_name)
+               
+        else:
+            # no local file, get access_token from website instead
+               
+            flow = dropbox.client.DropboxOAuth2FlowNoRedirect(key, secret)
+            authorize_url = flow.start()
+              
+            web = webbrowser.open(authorize_url, new=2, autoraise=True)
+            
+            access_token, user_id = flow.finish(code)
+            self.client = dropbox.client.DropboxClient(access_token)
+            print(access_token)
+       
+            # if db is empty too, new user w/ new install. create local xml file and push to db
+            if not client.search('//', file_name):
+                file = open(file_name, 'wb')
+                # TODO: file creation function goes here, placeholder write
+                # init_file(file)
+                file.write(access_token)
+                file.close()
+       
+                # for some reason open(file_name, 'r+') (read AND write) wouldn't work, so I had to
+                # open and close separately for the read and writes.
+                   
+                file = open(file_name, 'r')
+                client.put_file('//' + file_name, file)
+                file.close()
+            else:
+                # file on db, so this is an existing user w/ a new install
+                file = open(file_name, 'wb')
+                with client.get_file('//' + file_name) as db_file:
+                    file.write(db_file.read())
+                file.close()
+                """
+    
--- user_info_entry.py
+++ user_info_entry.py
@@ -3,65 +3,103 @@
 #needs a def to push/pull user information to text file
 from tkinter import *
 from tkinter import ttk
+import authenticate
 
-root = Tk()
-root.title("User Information")
+def finish_flow():
 
+    db.dropbox_finish(auth_code.get())
+    root.destroy()
 
-main_window = ttk.Frame(root, padding = "3 3 12 12")
-main_window.grid(column=0, row=0, stick = 'nesw')
-main_window.columnconfigure(0, weight =1)
-main_window.rowconfigure(0, weight= 1)
+db = authenticate.db()
+db.dropbox_start()
 
+if not db.client:
+    """
+    For the first sprint, we want to skip the login window if
+    there is already a character file present.
+    """
 
-#First name entry label, entry area, and entry variable
-fname = StringVar()
-f_name = ttk.Entry(main_window, width = 25, textvariable = fname)
-f_name.grid(row = 0, column = 1,columnspan = 3, sticky='e')
+    root = Tk()
+    root.title("User Information")
 
-f_name_label = ttk.Label(main_window, text = "First Name:", padding = 5)
-f_name_label.grid(row=0, column=0, sticky='w')
+    main_window = ttk.Frame(root, padding = "3 3 12 12")
+    main_window.grid(column=0, row=0, stick = 'nesw')
+    main_window.columnconfigure(0, weight =1)
+    main_window.rowconfigure(0, weight= 1)
+     
+     
+    #First name entry label, entry area, and entry variable
+    fname = StringVar()
+    f_name = ttk.Entry(main_window, width = 25, textvariable = fname)
+    f_name.grid(row = 0, column = 1,columnspan = 3, sticky='e')
+     
+    f_name_label = ttk.Label(main_window, text = "First Name:", padding = 5)
+    f_name_label.grid(row=0, column=0, sticky='w')
+     
+    #Last name entry label, entry area, and entry variable
+    lname = StringVar()
+    l_name = ttk.Entry(main_window, width = 25, textvariable = lname)
+    l_name.grid(row = 1, column = 1,columnspan = 3, sticky='e')
+     
+    l_name_label = ttk.Label(main_window, text = "Last Name:", padding = 5)
+    l_name_label.grid(row=1, column=0, sticky='w')
+     
+    #Character name entry label, entry area, and entry variable
+    character_name = StringVar()
+    character_name_entry = ttk.Entry(main_window, width = 25, textvariable = character_name)
+    character_name_entry.grid(row = 2, column = 1, columnspan = 3, sticky='e')
+     
+    character_name_label = ttk.Label(main_window, text = "Character (username):", padding = 5)
+    character_name_label.grid(row=2, column=0, sticky='w')
+     
+     
+    #Birthdate entry label, entry area, and entry variable                                        
+    birthdate_month = StringVar()
+    birthdate_month_entry = ttk.Entry(main_window, width = 2,
+                                      textvariable = birthdate_month)
+    birthdate_month_entry.grid(row = 3, column = 1, sticky = 'e')
+     
+    birthdate_day = StringVar()
+    birthdate_day_entry = ttk.Entry(main_window, width = 2,
+                                    textvariable = birthdate_day)
+    birthdate_day_entry.grid(row = 3, column = 2)
+     
+    birthdate_year = StringVar()
+    birthdate_year_entry = ttk.Entry(main_window, width = 2,
+                                     textvariable = birthdate_year)
+    birthdate_year_entry.grid(row = 3, column = 3, sticky = 'w')
+     
+    birthdate_label = ttk.Label(main_window, text = "Birthdate:", padding = 5)
+    birthdate_label.grid(row = 3, column = 0, sticky='w')
+     
+    #Area for dropbox connection information.
+    dropbox_frame = ttk.LabelFrame(main_window,
+                                   text = "Connect to DropBox Here",
+                                   height = 150, width = 300, padding = 10)
+    dropbox_frame.grid(row = 4, column = 0, columnspan = 4)
+      
+    line1 = '1. Go to your browser, a new tab should have opened:'
+    db_line1 = ttk.Label(dropbox_frame, text = line1, padding = 3)
+    db_line1.grid(row = 0, column = 0, sticky='w')
+     
+    line2 = '2. Click "Allow" (you may be asked to log in first)'
+    db_line2 = ttk.Label(dropbox_frame, text = line2, padding = 3)
+    db_line2.grid(row = 1, column = 0, sticky='w')
+     
+    line3 = '3. Copy the authorization code and paste below.'
+    db_line3 = ttk.Label(dropbox_frame, text = line3, padding = 3)
+    db_line3.grid(row = 2, column = 0, sticky='w')
+     
+    auth_code = StringVar()
+    code = ttk.Entry(dropbox_frame, width = 35, textvariable = auth_code)
+    code.grid(row = 3, column = 0)
+          
+    submit_button = Button(dropbox_frame, text = 'Submit', command = finish_flow)
+    submit_button.grid(row = 4, column = 0)
+      
+    root.mainloop()
 
-#Last name entry label, entry area, and entry variable
-lname = StringVar()
-l_name = ttk.Entry(main_window, width = 25, textvariable = lname)
-l_name.grid(row = 1, column = 1,columnspan = 3, sticky='e')
+else:
 
-l_name_label = ttk.Label(main_window, text = "Last Name:", padding = 5)
-l_name_label.grid(row=1, column=0, sticky='w')
-
-#Character name entry label, entry area, and entry variable
-character_name = StringVar()
-character_name_entry = ttk.Entry(main_window, width = 25, textvariable = character_name)
-character_name_entry.grid(row = 2, column = 1, columnspan = 3, sticky='e')
-
-character_name_label = ttk.Label(main_window, text = "Character Name:", padding = 5)
-character_name_label.grid(row=2, column=0, sticky='w')
-
-
-#Birthdate entry label, entry area, and entry variable                                        
-birthdate_month = StringVar()
-birthdate_month_entry = ttk.Entry(main_window, width = 2,
-                                  textvariable = birthdate_month)
-birthdate_month_entry.grid(row = 3, column = 1, sticky = 'e')
-
-birthdate_day = StringVar()
-birthdate_day_entry = ttk.Entry(main_window, width = 2,
-                                textvariable = birthdate_day)
-birthdate_day_entry.grid(row = 3, column = 2)
-
-birthdate_year = StringVar()
-birthdate_year_entry = ttk.Entry(main_window, width = 2,
-                                 textvariable = birthdate_year)
-birthdate_year_entry.grid(row = 3, column = 3, sticky = 'w')
-
-birthdate_label = ttk.Label(main_window, text = "Birthdate:", padding = 5)
-birthdate_label.grid(row=3, column=0, sticky='w')
-
-#Area for dropbox connection information.
-dropbox_frame = ttk.LabelFrame(main_window,
-                               text = "Connect to DropBox Here",
-                               height = 150, width = 300, padding = 10)
-dropbox_frame.grid(row=4, column = 0, columnspan = 4)
-
-root.mainloop()
+    # put main window here?
+    print('skipping login window, a local file is already present')
