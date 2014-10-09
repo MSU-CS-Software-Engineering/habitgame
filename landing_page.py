f1d9a4556d567f8fead0f939816f9dc0817909eb
cbrobbi <cbrobbi@gmail.com>
2014-10-09 18:16:38 -0500

    landing page to go with the gui that is now dictionary driven.

--- landing_page.py
+++ landing_page.py
@@ -4,48 +4,61 @@ from tkinter.ttk import *
 
 from tkinter import messagebox  #Must be explicitly imported. Used for placeholders.
 
-
+class Landing_Page (Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.landing_window()
+        self.columnconfigure(0, weight =1)
+        self.columnconfigure(2, weight = 1)
+        self.columnconfigure(4, weight = 1)
+        #self.columnconfigure(3, weight = 1)
+        #self.rowconfigure (4, weight =1)
+        #self.rowconfigure (5, weight = 1)
+        self.rowconfigure (6, weight = 1)
  
-def landing_window(self):
+    def landing_window(self):
 
+        #label above progress bar
+        progress_label = Label(self, text="Daily Progress")
+        progress_label.grid(row = 4, column =2 ,sticky='ew', pady=4, padx=5)
+        progress_label.configure(anchor = CENTER)
+        progress_label.rowconfigure(4, weight =1)
+        progress_label.columnconfigure(3, weight = 1)
 
-    progress_label = Label(self, text="Daily Progress")
-    progress_label.grid(row = 3, column =3 ,sticky=W+ E+S + N, pady=4, padx=5)
-    progress_label.configure(anchor = CENTER)
+        #progress bar
+        progress = Progressbar(self, orient = 'horizontal', mode= 'determinate')
+        progress.grid(row = 5, column=0, columnspan = 6, stick = 'ew', padx = 3)
+        progress.start()
+        progress.rowconfigure(5, weight =1)
+        progress.columnconfigure(0, weight = 1)
 
-    area1 = Canvas(self)
-    area1.grid(row=5, column=0, columnspan=2, rowspan=4, 
-        padx=5, sticky=E+W+S+N)
+        #three areas for adding dailies, task, habit widgets
+        area1 = Canvas(self)
+        area1.grid(row=6, column=0, columnspan=2, rowspan=4, 
+            padx=5, sticky='enws')
+        area1.rowconfigure(6, weight =1)
+        area1.columnconfigure(0, weight = 1)
 
-    area2 = Canvas(self)
-    area2.grid(row=5, column=2, columnspan=2, rowspan=4, 
-        padx=5, sticky=E+W+S+N)
+        area2 = Canvas(self)
+        area2.grid(row=6, column=2, columnspan=2, rowspan=4, 
+            padx=5, sticky='enws')
+        area2.rowconfigure(6, weight =1)
+        area2.columnconfigure(2, weight = 1)
 
-    area3 = Canvas(self)
-    area3.grid(row=5, column=4, columnspan=2, rowspan=4, 
-        padx=5, sticky=E+W+S+N)
+        area3 = Canvas(self)
+        area3.grid(row=6, column=4, columnspan=2, rowspan=4, 
+            padx=5, sticky=E+W+S+N)
+        area3.rowconfigure(6, weight =1)
+        area3.columnconfigure(4, weight = 1)
 
-    progress_label = Label(self, text="Daily Progress")
-    progress_label.grid(row = 3, column =3 ,sticky=W+ E+S + N, pady=4, padx=5)
-    progress_label.configure(anchor = CENTER)
-    
-    
-    progress = Progressbar(self, orient = 'horizontal', mode= 'determinate')
-    
-    progress.grid(row = 4, column=0, columnspan = 6, stick = E + W, padx = 3)
-    progress.start()
-              
-def start(self):
-    self.progress["value"] = 0
-    self.max = 24
-    self.progress["midnight"]=24
-    self.progress["value"] = 12
+        
+
+                  
+    def start(self):
+        self.progress["value"] = 0
+        self.max = 24
+        self.progress["midnight"]=24
+        self.progress["value"] = 12
   
-def main():
-  
-    pass 
-
-
-if __name__ == '__main__':
-    main()  
+ 
         
